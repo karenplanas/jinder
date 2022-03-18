2ed028e28d4db8724b28d626566c93ae34eff2a1
Sergihf10 <sergihf10@gmail.com>
2022-03-18 17:47:52 +0100

    implemented basic server structure

---
+++ server/models/db.ts
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connect = async () => {
+  console.log('Connecting to database');
+  try {
+    await mongoose.connect('mongodb://127.0.0.1/jinder');
+    console.log('Connected successfully to database');
+  } catch (e) {
+    console.error('Error connecting to database', e);
+    throw e;
+  }
+};
+
+export {connect};
