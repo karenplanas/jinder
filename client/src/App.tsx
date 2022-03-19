43e974b67988ccf115e3628131cfcd5b2e0fb239
Sergihf10 <sergihf10@gmail.com>
2022-03-19 11:13:20 +0100

    Created joboffer component and interfaces folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import CreateJobOffer from './components/CreateJob/CreateJobOffer';
 
 function App() {
   return (
     <div className="App">
-      {" "}
       <h1> Jinder!! be a slut to get a job </h1>
+      <CreateJobOffer></CreateJobOffer>
     </div>
   );
 }
