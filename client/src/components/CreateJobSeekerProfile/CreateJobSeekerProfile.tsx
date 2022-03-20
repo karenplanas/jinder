9e81d9542d935d0c9658121240c461893c51ec5d
Karen <lakarina86@gmail.com>
2022-03-20 15:51:18 +0100

    style: add some style to jobseeker

--- client/src/components/CreateJobSeekerProfile/CreateJobSeekerProfile.tsx
+++ client/src/components/CreateJobSeekerProfile/CreateJobSeekerProfile.tsx
@@ -1,19 +1,64 @@
 import React from 'react'
 import { ProfileTabsNav } from '../ProfileTabsNav/ProfileTabsNav'
+import './CreateJobSeekerProfile.css'
 
 const CreateJobSeekerProfile: React.FC = () => {
 
   return (
     <div>
       <ProfileTabsNav />
-      <div className='CreateJobSeekerProfile-Experience'>
-        <h3>Experience</h3>
-        <input placeholder='Title'/>
-        <input placeholder='Company Name'/>
-        <input placeholder='Location'/>
-        <input placeholder='Start Date'/>
-        <input placeholder='End Date'/>
-        <input placeholder='Job Description / Tasks performed'/>
+      <div>
+        <div className='CreateJobSeekerProfile-Experience profile-sections'>
+          <h3>Experience</h3>
+          <input placeholder='Title'/>
+          <input placeholder='Company Name'/>
+          <input placeholder='Location'/>
+          <input placeholder='Start Date'/>
+          <input placeholder='End Date'/>
+          <input placeholder='Job Description / Tasks performed'/>
+        </div>
+        <div className='CreateJobSeekerProfile-Skills profile-sections'>
+          <h3>Skills</h3>
+          <div className='Skills-checks'>
+            <div>
+              <input type="checkbox" name='C++'/>
+              <label htmlFor='C++'>C++</label>
+            </div>
+            <div>
+              <input type="checkbox" name='JS'/>
+              <label htmlFor='C++'>JS</label>
+            </div>
+            <div>
+              <input type="checkbox" name='TS'/>
+              <label htmlFor='C++'>TS</label>
+            </div>
+          </div>
+          <div className='others'>
+            <label htmlFor='others'>Others</label>
+            <input name='others' placeholder='Type something here...'/>
+          </div>
+        </div>
+        <div className='CreateJobSeekerProfile-LookingFor profile-sections'>
+          <h3>Looking For</h3>
+          <div className='LookingFor-checks'>
+            <div>
+              <input type="checkbox" name='Front-end'/>
+              <label htmlFor='Front-end'>Front-end</label>
+            </div>
+            <div>
+              <input type="checkbox" name='Back-end'/>
+              <label htmlFor='Back-end'>Back-end</label>
+            </div>
+            <div>
+              <input type="checkbox" name='Full-stack'/>
+              <label htmlFor='Full-stack'>Full-stack</label>
+            </div>
+          </div>
+          <div className='others'>
+            <label htmlFor='others'>Others</label>
+            <input name='others' placeholder='Type something here...'/>
+          </div>
+        </div>
         <button>Save</button>
       </div>
     </div>
--- client/src/components/ProfileTabsNav/ProfileTabsNav.tsx
+++ client/src/components/ProfileTabsNav/ProfileTabsNav.tsx
@@ -4,9 +4,9 @@ import './ProfileTabsNav.css'
 const ProfileTabsNav: React.FC = () => {
   return (
     <div className='ProfileTabsNav-container'>
-      <h3>Experience</h3>
-      <h3>Skills</h3>
-      <h3>Looking For</h3>
+      <div><h3>Experience</h3></div>
+      <div><h3>Skills</h3></div>
+      <div><h3>Looking For</h3></div>
     </div>
   )
 }
