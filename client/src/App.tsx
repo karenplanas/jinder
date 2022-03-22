a745451f658f4d533a8b9f8d3a97bd20f4f44f84
Karen <lakarina86@gmail.com>
2022-03-22 11:45:54 +0100

    feat:add routes for jobseeker profile creation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,7 +17,6 @@ const App: React.FC = () => {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/home" element={<JobList />} />
         <Route path="/job-position/edit" element={<CreateJobOffer />} />
-
         <Route path="/job-seeker-profile/edit/experience" element={<CreateJobSeekerProfile />} />
         <Route path="/job-seeker-profile/edit/skills" element={<JobSeekerProfileSkills />} />
         <Route path="/job-seeker-profile/edit/looking-for" element={<JobSeekerProfileLookingFor />} />
--- client/src/components/CreateJobSeekerProfile/JobSeekerProfileLookingFor.tsx
+++ client/src/components/CreateJobSeekerProfile/JobSeekerProfileLookingFor.tsx
@@ -1,38 +1,46 @@
 import React from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
 import { Checkbox } from '../Checkbox/Checkbox'
 import { PlusInCircle } from '../icons/PlusInCircle'
 import { InputTextField } from '../InputTextField/InputTextField'
 
 const JobSeekerProfileLookingFor: React.FC = () => {
+
+  const methods = useForm();
+
   return (
-    <div className='CreateJobSeekerProfile-LookingFor profile-sections'>
-      <div className='title-and-plus'>
-        <h3>Looking For</h3>
-        <PlusInCircle />
-      </div>
-      <div className='checks'>
+    <FormProvider {...methods}>
+      <form>
+        <div className='CreateJobSeekerProfile-LookingFor profile-sections'>
+          <div className='title-and-plus'>
+            <h3>Looking For</h3>
+            <PlusInCircle />
+          </div>
+          <div className='checks'>
 
-        <div className='checks-line'>
-          <Checkbox name='skills' value='frontEnd' label='Front-end'/>
-          <Checkbox name='skills' value='backEnd' label='Back-end'/>
-          <Checkbox name='skills' value='fullStack' label='Full-stack'/>
+            <div className='checks-line'>
+              <Checkbox name='lookingFor' value='frontEnd' label='Front-end'/>
+              <Checkbox name='lookingFor' value='backEnd' label='Back-end'/>
+              <Checkbox name='lookingFor' value='fullStack' label='Full-stack'/>
+            </div>
+
+            <div className='checks-line'>
+              <Checkbox name='lookingFor' value='junior' label='Junior'/>
+              <Checkbox name='lookingFor' value='senior' label='Senior'/>
+              <Checkbox name='lookingFor' value='pm' label='Project Manager'/>
+            </div>
+
+            <div className='checks-line'>
+              <Checkbox name='lookingFor' value='remote' label='Remote'/>
+              <Checkbox name='lookingFor' value='fullTime' label='Full time'/>
+              <Checkbox name='lookingFor' value='partTime' label='Part time'/>
+            </div>
+
+          </div>
+          <InputTextField name='others-looking-for' placeholder='Type something here...' label='Other' />
         </div>
-
-        <div className='checks-line'>
-          <Checkbox name='skills' value='junior' label='Junior'/>
-          <Checkbox name='skills' value='senior' label='Senior'/>
-          <Checkbox name='skills' value='pm' label='Project Manager'/>
-        </div>
-
-        <div className='checks-line'>
-          <Checkbox name='skills' value='remote' label='Remote'/>
-          <Checkbox name='skills' value='fullTime' label='Full time'/>
-          <Checkbox name='skills' value='partTime' label='Part time'/>
-        </div>
-
-      </div>
-      <InputTextField name='others-looking-for' placeholder='Type something here...' label='Other' />
-    </div>
+      </form>
+    </FormProvider>
   )
 }
 
