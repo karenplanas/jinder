c27718005efbc8f4f7e1f5a26e2b2b32994351ee
Karen <lakarina86@gmail.com>
2022-03-23 12:32:03 +0100

    feat: add db connection URI on backend

--- server/src/router.ts
+++ server/src/router.ts
@@ -4,8 +4,8 @@ import * as jobOfferController from './controllers/JobOffer-controller';
 const router = express.Router();
 
 // https://stackoverflow.blog/2020/03/02/best-practices-for-rest-api-design/
-// POST /job-offers
-// GET /job-offers
+// POST /job-postings
+// GET /job-postings
 router.post('/newJobOffer', jobOfferController.postJobOffer);
 router.get('/JobOffers', jobOfferController.getJobOffers);
 
