d74963fc885a88ac15e29702dfe35f46e962d399
Karen <lakarina86@gmail.com>
2022-03-28 22:38:52 +0200

    feat: add chat fetching calls to authenticated-api-client

---
+++ client/src/Interfaces/Chat.ts
@@ -0,0 +1,8 @@
+import { Message } from "./Message"
+import { Message } from "./Message"
+
+export interface Chat {
+  jobSeekerUserId: string
+  employerUserId: string
+  messages: Message[]
+}
--- client/src/services/authenticated-api-client.ts
+++ client/src/services/authenticated-api-client.ts
@@ -1,6 +1,8 @@
 import { useUserContext } from "../contexts/UserContext"
+import { Chat } from "../Interfaces/Chat"
 import { JobOffer } from "../Interfaces/JobOffer"
 import { JobSeekerProfile } from "../Interfaces/JobSeekerProfile"
+import { Message } from "../Interfaces/Message"
 import { UserJobOffer } from "../Interfaces/UserJobOffer"
 import { performRequest } from "./helpers"
 
@@ -49,8 +51,35 @@ export const useAuthenticatedApiClient = () => {
     })
   }
 
+  const getChats = (): Promise<Chat[]> => {
+    return performRequest({
+      method: 'GET',
+      path: '/chats',
+      token: user?.accessToken
+    })
+  }
+
+  const postChat = (payload: Partial<Chat>): Promise<Chat> => {
+    return performRequest({
+      method: 'POST',
+      path: '/chats',
+      token: user?.accessToken
+    })
+  }
+
+  const postMessage = (chatId: string, payload: Partial<Message>): Promise<Chat> => {
+    return performRequest({
+      method: 'POST',
+      path: `/chats/${chatId}/messages`,
+      token: user?.accessToken
+    })
+  }
+
   return {
     dislikeJobOffer,
+    getChats,
+    postChat,
+    postMessage,
     getJobOffers,
     getLikedJobOffers,
     likeJobOffer,
