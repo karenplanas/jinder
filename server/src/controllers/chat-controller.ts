ba36832edb9edee11ebd8f43fbdc4b6b557d3f44
Karen <lakarina86@gmail.com>
2022-03-28 20:04:52 +0200

    feat: add chat model/controller

---
+++ server/src/controllers/chat-controller.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import * as Chat from '../models/ChatSchema'
+
+const getChats = async (req: express.Request, res: express.Response) => {
+  const response = await Chat.getChats()
+  res.status(200)
+  res.json({ data: response})
+}
+
+const postChat = async (req: express.Request, res: express.Response) => {
+  const response = await Chat.createChat(req.user!._id, req.params.id, req.body )
+  res.status(200)
+  res.json({ data: response})
+}
+
+export { getChats, postChat }
+
+
+
---
+++ server/src/models/ChatSchema.ts
@@ -0,0 +1,51 @@
+import { timeStamp } from "console";
+import mongoose from "mongoose";
+import { Schema, model } from "mongoose";
+
+interface Chat {
+  jobSeekerUserId : mongoose.Types.ObjectId
+  employerUserId: mongoose.Types.ObjectId
+  messages : Message[] 
+}
+
+interface Message {
+  senderId: mongoose.Types.ObjectId
+  content: string
+  sentAt: Date
+}
+
+const MessageSchema = new Schema<Message>({
+  senderId: { type: mongoose.SchemaTypes.ObjectId},
+  content: { type: String },
+  sentAt: { type: mongoose.SchemaTypes.Date}
+  }, 
+  { timestamps: true }
+)
+
+const ChatSchema = new Schema<Chat>({
+  jobSeekerUserId: { type: mongoose.SchemaTypes.ObjectId },
+  employerUserId: { type: mongoose.SchemaTypes.ObjectId },
+  messages: { type: [MessageSchema]}
+})
+
+const Chat = model('chat', ChatSchema)
+const Message = model('message', MessageSchema)
+
+const getChats = () => {
+  return Chat.find()
+}
+
+const createChat = (
+  jobSeekerUserId: string, 
+  employerUserId: string,
+  payload: Message[]
+  ) => {
+    return Chat.findOneAndUpdate(
+      { jobSeekerUserId, employerUserId }, 
+      { $set: payload }, 
+      { upsert: true }
+    )
+}
+
+export { Chat, Message, getChats, createChat }
+
